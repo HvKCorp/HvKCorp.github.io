bc4ccb1a4bbc3c674ab98eca013e6f0db81603ab
The Legendary Roadside Havoc <92639283+Roadside-Havoc@users.noreply.github.com>
2023-03-04 14:50:34 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import { BreakoutGame } from "./breakout-game.js";
-import BreakoutGame from "@glitchkey/breakout-game";
-const canvas = document.getElementById("breakout");
-var game = new BreakoutGame(canvas, 800, 600);
-game.start();
