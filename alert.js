8dd1c8e3bef2830cb085db391d7b1c84a6706dd1
HvK Co <92639283+HvK-Corp@users.noreply.github.com>
2023-05-05 19:08:09 +0530

    Delete alert.js

--- alert.js
+++
@@ -1,8 +0,0 @@
-function callback(){
-  alert(window.orientation);
-  if(window.innerHeight > window.innerWidth){
-    alert("Rotate the device for best experience")
-  }
-}
-
-window.addEventListener('orientationchange', callback, true);
